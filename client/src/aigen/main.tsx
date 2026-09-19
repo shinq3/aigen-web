a4a2e03278cd7119b7db9f70e20ace37dbac7fc1
AIGen One CMS Runner <runner@aigen-one.local>
2026-09-19 05:06:23 +0000

    CMS preview fa28d8c9

--- client/src/aigen/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import AigenApp from "./AigenApp";
-import "@/index.css";
-import "./i18n";
-
-createRoot(document.getElementById("root")!).render(<AigenApp />);
